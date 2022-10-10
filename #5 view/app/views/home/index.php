9a14b78f5556e7a8cbdc67f291009f8cbc783f85
FikriNurhakimAkbar <nurhakimf2906@gmail.com>
2022-10-10 14:18:54 +0700

    Menambahkan view

---
+++ #5 view/app/views/home/index.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <tittle>Halaman Home</tittle>
+</head>
+<body>
+    <h1>Selamat Datang di Website Saya</h1>
+</body>
+</html>
---
+++ #5 view/app/views/templates/header.php
@@ -0,0 +1,7 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <tittle>Halaman <? $data['judul']; ?></tittle>
+</head>
+<body>
