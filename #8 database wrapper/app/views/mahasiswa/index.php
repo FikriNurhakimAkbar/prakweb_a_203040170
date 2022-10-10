ccac4067b763ae975264488ad6c112077f6fffe6
FikriNurhakimAkbar <nurhakimf2906@gmail.com>
2022-10-10 14:52:07 +0700

    Menambahkan Database Wrapper

---
+++ #8 database wrapper/app/views/mahasiswa/index.php
@@ -0,0 +1,20 @@
+<div class="container"mt-5>
+
+<div class="row">
+    <div class="col -6">
+        <h3>Daftar Mahasiswa</h3>
+        <ul class="list-group">
+            <?php foreach( $data['mhs'] as $mhs ) : ?>
+            <li class="list-group-item">
+                <?= $mhs['nama']; ?>
+                <a href="<? BASEURL; ?>/mahasiswa/detail/<? $mhs['id']; ?>" class="badge badge-primary>detail</a>
+        </li>
+            <?php endforeach; ?>            
+        </ul>
+            
+               
+           
+        
+    </div>
+</div>
+</div>
